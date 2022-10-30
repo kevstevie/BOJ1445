784b7148dcf3ad011e644940f5146294f12f05fd
kevstevie <jianna442@gmail.com>
2022-10-30 19:15:53 +0900

    feat: add make maps to contain near garbage info

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
@@ -11,12 +12,42 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        String[][] maps = new String[n][m];
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        char[][] maps = new char[n][m];
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+            String str = br.readLine();
             for (int j = 0; j < m; j++) {
-                maps[i][j] = st.nextToken();
+                maps[i][j] = str.charAt(j);
+            }
+        }
+
+        makeNearGarbage(maps);
+
+        
+    }
+
+    static int n;
+    static int m;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static void makeNearGarbage(char[][] maps) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (maps[i][j] == '.') {
+                    for (int dir = 0; dir < 4; dir++) {
+                        int x = i + dx[dir];
+                        int y = j + dy[dir];
+                        if ( x < 0 || x >= n ||
+                                y < 0 || y >= m || maps[x][y] != 'g') {
+                            continue;
+                        }
+                        maps[i][j] = 'n';
+                    }
+                }
             }
         }
     }
